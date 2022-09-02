ef99adcfc9b858730562bbce7c66e03f91c2a592
Alex Alexandre Alves <alexalexandrealves@gmail.com>
2022-09-01 22:54:07 -0300

    update css

---
+++ src/pages/App.tsx
@@ -0,0 +1,14 @@
+  import Formulario from "../components/forms";
+  import Lista from "../components/list";
+  import './style.scss';
+
+  function App() {
+    return (
+      <div className = "AppStyle">
+        <Formulario/>
+        <Lista/>
+      </div>
+    );
+  }
+
+  export default App;
